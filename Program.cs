f2b98785c556939a9060f875f875903023f94386
Artem <temass2012@gmail.com>
2023-02-17 22:50:16 +0300

    Объявил переменные

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//string[] array = {"lol","kek","cheburek",":)","123"};
+
+System.Console.WriteLine("Введите слова через пробел: ");
+string[] array =  Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+int count = 0;
+string[] outArr = new string[array.Length];
