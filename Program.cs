bcad71e438f748b1253393fbe1cb581d18ef14e7
Artem <temass2012@gmail.com>
2023-02-17 22:56:02 +0300

    алгоритм готов

--- Program.cs
+++ Program.cs
@@ -4,3 +4,15 @@ System.Console.WriteLine("Введите слова через пробел: ");
 string[] array =  Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
 int count = 0;
 string[] outArr = new string[array.Length];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        outArr[count] = array[i];
+        count++;
+    }
+
+}
+Array.Resize(ref outArr,count);
+System.Console.WriteLine("[{0}]", String.Join(",", outArr));
